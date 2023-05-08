5d89454ab2361dd2c93acce48ad78c9de01cc05f
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-08 11:09:55 -0600

    Defining DynamoDB Routes

---
+++ routes/ddbMusicRoutes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Use Express
+const express = require('express');
+const ddbMusicApi = express.Router();
+
+// Import Queries
+const {
+    listMusic,
+    getMusic,
+    postMusic,
+    deleteMusic,
+} = require('../helpers/dynamoDbQueries.js');
+
+
+// Photo API Routes
+ddbMusicApi.get('/', listMusic);
+ddbMusicApi.get('/:name', getMusic);
+ddbMusicApi.post('/upload', postMusic);
+ddbMusicApi.delete('/:name', deleteMusic);
+
+// Export API
+module.exports = ddbMusicApi;
