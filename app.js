84570a23422e15ea8458f9920ca1823d3fb35c1d
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-08 11:17:23 -0600

    Fixing linting on s3Music routes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 
 // Use Body Parser
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,29 +23,32 @@ const session = require('express-session');
 const storeSession = new session.MemoryStore(); //Dev Only Move to DB for Prod Sessions
 
 app.use(
-	session({
-		secret: SESSION_SECRET, 
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 24, 
-			secure: false,
-			sameSite: 'none'
-		},
-		resave: false,
-		saveUninitialized: false,
-		storeSession
-	})
+    session({
+        secret: SESSION_SECRET, 
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24, 
+            secure: false,
+            sameSite: 'none'
+        },
+        resave: false,
+        saveUninitialized: false,
+        storeSession
+    })
 );
 
 // Service Routers
 const s3MusicRouter = require('./routes/s3MusicRoutes.js');
 app.use('/musicrepo', s3MusicRouter);
 
+const ddbMusicRouter = require('./routes/ddbMusicRoutes.js');
+app.use('/route', ddbMusicRouter);
+
 // Define Port
 const serverPort = process.env.PORT || 3000;
 
 // Initialize Server
 app.listen(serverPort, () => {
-	console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort); 
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort); 
 });
 
 // Export App
