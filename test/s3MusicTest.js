10bf9795c863528c523cd0d5421e61ddc805bed6
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-08 11:38:15 -0600

    Fixing linting on s3Music tests

--- test/s3MusicTest.js
+++ test/s3MusicTest.js
@@ -5,254 +5,249 @@ const app = require('../app.js');
 
 //Test: GET /musicrepo
 describe('GET /musicrepo', () => {
-	it('status_code: 200', async () => {
-		// Setup
-		const excerciseUrl = '/musicrepo';
-		const expected = 200;
+    it('status_code: 200', async () => {
+        // Setup
+        const excerciseUrl = '/musicrepo';
+        const expected = 200;
 
-		// Exercise
-		const response = await request(app)
-			.get(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
 
-		const result = response.status;
+        const result = response.status;
 
-		// Verify
-		assert.equal(result, expected);
-	});
+        // Verify
+        assert.equal(result, expected);
+    });
 
-	it('Status: Success', async () => {    
-		// Setup
-		const excerciseUrl = '/musicrepo';
-		const expected = 'Success';
+    it('Status: Success', async () => {    
+        // Setup
+        const excerciseUrl = '/musicrepo';
+        const expected = 'Success';
 
-		// Exercise
-		const response = await request(app)
-			.get(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
 
-		const result = response._body.status;
+        const result = response._body.status;
 
-		// Verify
-		assert.equal(result, expected);
-	});
+        // Verify
+        assert.equal(result, expected);
+    });
 });
 
 //Test: GET /musicrepo/:name 
 describe('GET /musicrepo/:name', () => {
-	it('status_code: 200', async () => { 
-		// Setup
-        const file = 'Test.m4a'
-        const path = './test/'
-        const setupUrl = '/musicrepo/upload'
-        const excerciseUrl = `/musicrepo/${file}`
-		const expected = '200';
+    it('status_code: 200', async () => { 
+        // Setup
+        const file = 'Test.m4a';
+        const path = './test/';
+        const setupUrl = '/musicrepo/upload';
+        const excerciseUrl = `/musicrepo/${file}`;
+        const expected = '200';
 
         await request(app)
             .post(setupUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-            'name': file,
-            'path': path
-        });    
+                'name': file,
+                'path': path
+            });    
 
-		// Exercise
-		const response = await request(app)
-			.get(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
 
-		const result = response.status;
+        const result = response.status;
 
-		// Verify
-		assert.equal(result, expected);
-
-		//Teardown
-		const teardownUrl = `/musicrepo/${file}`;
+        // Verify
+        assert.equal(result, expected);
+    
+	    //Teardown
+        const teardownUrl = `/musicrepo/${file}`;
  
-		await request(app)
-			.delete(teardownUrl);
+        await request(app)
+            .delete(teardownUrl);
+    }).timeout(5000);
 
-	}).timeout(5000);
-
-	it('Status: Success', async () => { 
-		// Setup
-        const file = 'Test.m4a'
-        const path = './test/'
-        const setupUrl = '/musicrepo/upload'
-        const excerciseUrl = `/musicrepo/${file}`
-		const expected = 'Success';
+    it('Status: Success', async () => { 
+        // Setup
+        const file = 'Test.m4a';
+        const path = './test/';
+        const setupUrl = '/musicrepo/upload';
+        const excerciseUrl = `/musicrepo/${file}`;
+        const expected = 'Success';
 
         await request(app)
             .post(setupUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-            'name': file,
-            'path': path
-        });    
+                'name': file,
+                'path': path
+            });    
 
-		// Exercise
-		const response = await request(app)
-			.get(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
 
-			const result = response._body.status;
+        const result = response._body.status;
 
-		// Verify
-		assert.equal(result, expected);
+        // Verify
+        assert.equal(result, expected);
 
-		//Teardown
-		const teardownUrl = `/musicrepo/${file}`;
+        //Teardown
+        const teardownUrl = `/musicrepo/${file}`;
  
-		await request(app)
-			.delete(teardownUrl);
+        await request(app)
+            .delete(teardownUrl);
+    }).timeout(5000);
 
-	}).timeout(5000);
-
-	it('Validate Returning PreSigned URL', async () => { 
-		// Setup
-        const file = 'Test.m4a'
-        const path = './test/'
-        const setupUrl = '/musicrepo/upload'
-        const excerciseUrl = `/musicrepo/${file}`
-		const expected = 'https://music-is-life-bucket-s3bucket-1p1cgsnuuvm73.s3.us-east-1.amazonaws.com/Test.m4a';
-		const regex = /https:\/\/music-is-life-bucket-s3bucket-1p1cgsnuuvm73\.s3\.us-east-1\.amazonaws\.com\/Test\.m4a/i
+    it('Validate Returning PreSigned URL', async () => { 
+        // Setup
+        const file = 'Test.m4a';
+        const path = './test/';
+        const setupUrl = '/musicrepo/upload';
+        const excerciseUrl = `/musicrepo/${file}`;
+        const expected = 'https://music-is-life-bucket-s3bucket-1p1cgsnuuvm73.s3.us-east-1.amazonaws.com/Test.m4a';
+        const regex = /https:\/\/music-is-life-bucket-s3bucket-1p1cgsnuuvm73\.s3\.us-east-1\.amazonaws\.com\/Test\.m4a/i;
 
         await request(app)
             .post(setupUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-            'name': file,
-            'path': path
-        });    
+            	'name': file,
+            	'path': path
+        	});    
 
-		// Exercise
-		const response = await request(app)
-			.get(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
 
-			const result = response._body.data;
+        const result = response._body.data;
 
-		// Verify
-		assert.match(result, regex, expected);
+        // Verify
+        assert.match(result, regex, expected);
 
-		//Teardown
-		const teardownUrl = `/musicrepo/${file}`;
+        //Teardown
+        const teardownUrl = `/musicrepo/${file}`;
  
-		await request(app)
-			.delete(teardownUrl);
-
-	}).timeout(5000);
+        await request(app)
+            .delete(teardownUrl);
+    }).timeout(5000);
 });
 
 //Test: Post /musicrepo/upload
 describe('POST /musicrepo/upload', () => {
-	it('status_code: 200', async () => { 
-		// Setup
-		const excerciseUrl = '/musicrepo/upload'
-        const file = 'Test.m4a'
-        const path = './test/'
-		const expected = 200;
+    it('status_code: 200', async () => { 
+        // Setup
+        const excerciseUrl = '/musicrepo/upload';
+        const file = 'Test.m4a';
+        const path = './test/';
+        const expected = 200;
 
-		// Exercise
+        // Exercise
         const response = await request(app)
             .post(excerciseUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-              'name': file,
-              'path': path
+        		'name': file,
+            	'path': path
             });    
 
-		const result = response.status;
+        const result = response.status;
 
-		// Verify
-		assert.equal(result, expected);
+        // Verify
+        assert.equal(result, expected);
 
-		//Teardown
-		const teardownUrl = `/musicrepo/${file}`;
+        //Teardown
+        const teardownUrl = `/musicrepo/${file}`;
         
-		await request(app)
-			.delete(teardownUrl);
+        await request(app)
+            .delete(teardownUrl);
+    }).timeout(5000);
 
-	}).timeout(5000);
+    it('Status: OK', async () => {  
+        // Setup
+        const excerciseUrl = '/musicrepo/upload';
+        const file = 'Test.m4a';
+        const path = './test/';
+        const expected = 'OK';
 
-	it('Status: OK', async () => {  
-		// Setup
-		const excerciseUrl = '/musicrepo/upload'
-        const file = 'Test.m4a'
-        const path = './test/'
-		const expected = 'OK';
-
-		// Exercise
+        // Exercise
         const response = await request(app)
             .post(excerciseUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-              'name': file,
-              'path': path
+        		'name': file,
+            	'path': path
             });    
 
-		const result = response.res.statusMessage;
+        const result = response.res.statusMessage;
 
-		// Verify
-		assert.equal(result, expected);
+        // Verify
+        assert.equal(result, expected);
 
-		//Teardown
-		const teardownUrl = `/musicrepo/${file}`;
+        //Teardown
+        const teardownUrl = `/musicrepo/${file}`;
  
-		await request(app)
-			.delete(teardownUrl);
-
-	}).timeout(5000);  
+        await request(app)
+            .delete(teardownUrl);
+    }).timeout(5000);  
 });
 
 //Test: DELETE /musicrepo/:name
 describe('DELETE /route/photos/:id', () => {
-	it('status_code: 200', async () => { 
-		// Setup
-        const file = 'Test.m4a'
-        const path = './test/'
-        const setupUrl = '/musicrepo/upload'
-        const excerciseUrl = `/musicrepo/${file}`
+    it('status_code: 200', async () => { 
+        // Setup
+        const file = 'Test.m4a';
+        const path = './test/';
+        const setupUrl = '/musicrepo/upload';
+        const excerciseUrl = `/musicrepo/${file}`;
 
         await request(app)
             .post(setupUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-            'name': file,
-            'path': path
-        });    
+            	'name': file,
+            	'path': path
+        	});    
 
-		const expected = 200;
+        const expected = 200;
 
-		// Exercise
-		const response = await request(app)
-			.delete(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .delete(excerciseUrl);
 
-		const result = response.status;
+        const result = response.status;
 
-		// Verify
-		assert.equal(result, expected);
-	}).timeout(5000);
+        // Verify
+        assert.equal(result, expected);
+    }).timeout(5000);
 
-	it('Status: Success', async () => {  
-		// Setup
-        const file = 'Test.m4a'
-        const path = './test/'
-        const setupUrl = '/musicrepo/upload'
-        const excerciseUrl = `/musicrepo/${file}`
+    it('Status: Success', async () => {  
+        // Setup
+        const file = 'Test.m4a';
+        const path = './test/';
+        const setupUrl = '/musicrepo/upload';
+        const excerciseUrl = `/musicrepo/${file}`;
 
         await request(app)
             .post(setupUrl)
             .set('Content-Type', 'application/x-www-form-urlencoded')
             .send({
-            'name': file,
-            'path': path
-        });    
+            	'name': file,
+            	'path': path
+        	});    
 
-		const expected = 'Success';
+        const expected = 'Success';
 
-		// Exercise
-		const response = await request(app)
-			.delete(excerciseUrl);
+        // Exercise
+        const response = await request(app)
+            .delete(excerciseUrl);
 
-		const result = response._body.status;
+        const result = response._body.status;
 
-		// Verify
-		assert.equal(result, expected);
-	}).timeout(5000);
+        // Verify
+        assert.equal(result, expected);
+    }).timeout(5000);
 });
