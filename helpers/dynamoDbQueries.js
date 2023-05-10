c8fa50c9717724bc0a18dfaae475e15d5a850f44
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-10 08:09:35 -0600

    Adding DynamoDB List API

--- helpers/dynamoDbQueries.js
+++ helpers/dynamoDbQueries.js
@@ -23,8 +23,6 @@ const client = new DynamoDBClient({
     region: 'us-east-1',
 });
 
-
-
 const listMusic = async (request, response) => {
     const listObjects = new ScanCommand({
         'TableName': 'Music-Is-Life',
