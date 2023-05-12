ea52385bbb56297bc9cf3b0ff4c9a30564423764
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-12 12:03:54 -0600

    Creating apiMusic Test file

--- test/apiMusicTest.js
+++ test/apiMusicTest.js
@@ -2,3 +2,36 @@
 const request = require('supertest');
 const assert = require('assert');
 const app = require('../app.js');
+
+//Test: GET /api
+describe('GET /api', () => {
+    it('status_code: 200', async () => {
+        // Setup
+        const excerciseUrl = '/db';
+        const expected = 200;
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Status: Success', async () => {    
+        // Setup
+        const excerciseUrl = '/db';
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+});
