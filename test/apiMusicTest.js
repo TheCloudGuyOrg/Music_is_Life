69fa83c89b975e25fbea5501fa7a8cc45fd6238b
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-05-12 12:18:29 -0600

    Creating apiMusic Test file

--- test/apiMusicTest.js
+++ test/apiMusicTest.js
@@ -34,4 +34,248 @@ describe('GET /api', () => {
         // Verify
         assert.equal(result, expected);
     });
+
+    it('Validate Database Retrieval', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+});
+
+describe('GET /api/:name', () => {
+    it('status_code: 200', async () => {
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 200;
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Status: Success', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate Database Retrieval', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+});
+
+describe('GET /api/url/:name', () => {
+    it('status_code: 200', async () => {
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/url/${name}`;
+        const expected = 200;
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Status: Success', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/url/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate Returning PreSigned URL', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/url/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+});
+
+describe('POST /api', () => {
+    it('status_code: 200', async () => {
+        // Setup
+        const excerciseUrl = '/api';
+        const expected = 200;
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Status: Success', async () => {    
+        // Setup
+        const excerciseUrl = '/api';
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate DB Entry', async () => {    
+        // Setup
+        const excerciseUrl = '/api';
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate S3 Entry', async () => {    
+        // Setup
+        const excerciseUrl = '/api';
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+});
+
+describe('DELETE /api/:name', () => {
+    it('status_code: 200', async () => {
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 200;
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Status: Success', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate DB Removal', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
+
+    it('Validate S3 Removal', async () => {    
+        // Setup
+        const name = '';
+        const excerciseUrl = `/api/${name}`;
+        const expected = 'Success';
+
+        // Exercise
+        const response = await request(app)
+            .get(excerciseUrl);
+
+        const result = response._body.status;
+
+        // Verify
+        assert.equal(result, expected);
+    });
 });
